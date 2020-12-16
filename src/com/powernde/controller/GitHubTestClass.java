1479d0004ead03cc3e250168a3e7b631c5b901f5
WangHeng <1359344826@qq.com>
2020-12-16 17:21:22 +0800

    用来测试gutHub的push and pull

---
+++ src/com/powernde/controller/GitHubTestClass.java
@@ -0,0 +1,15 @@
+package com.powernde.controller;
+
+/**
+ * @ProjectName: SpringMvcProject
+ * @Package: com.powernde.controller
+ * @Description: java类作用描述
+ * @Author: 王恒
+ * @CreateDate: 2020/12/16 17:19
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class GitHubTestClass {
+
+}
